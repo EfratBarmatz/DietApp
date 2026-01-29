e1550c9cd2fc38d85c22a58f19260451d9e8f797
EfratBarmatz <e0556737828@gmail.com>
2026-01-29 12:46:17 +0200

    Update User.cs more dateils

--- Models/User.cs
+++ Models/User.cs
@@ -6,4 +6,11 @@ public class User
     public string Email { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public string Name { get; set; } = "משתמש/ת";
+    public int Age { get; set; }
+    public double Weight { get; set; } 
+    public int Height { get; set; } 
+    public string Gender { get; set; } = "female"; 
+    public double ActivityLevel { get; set; } = 1.2; 
+    public int DailyCalorieGoal { get; set; } = 1500; 
 }
