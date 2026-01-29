9e1a101960e041ed9d8da9e6aee5576db44257c6
EfratBarmatz <e0556737828@gmail.com>
2026-01-29 12:47:02 +0200

    Update Dtos.cs updaeUser

--- Models/Dtos.cs
+++ Models/Dtos.cs
@@ -11,3 +11,14 @@ public class UserLoginDto
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
+
+public class UserProfileDto
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Weight { get; set; }
+    public int Height { get; set; }
+    public string Gender { get; set; }
+    public double ActivityLevel { get; set; }
+    public int ManualGoal { get; set; }
+}
